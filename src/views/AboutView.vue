9c7e7e362fdbf14dbab2945d2a3d1b46d97151db
Bestian Tang 小巴 <bestian@gmail.com>
2022-09-30 15:35:25 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>小太陽作品集</h1>
+    <div class="ui segment">
+      <p>耀洋的小名叫作小太陽。是一個活潑、溫暖、開朗的小孩，健康靈活，喜歡和人互動相處，無論是小小孩、同齡朋友、青年、成人、老人，他都樂於相處互動。</p>
+    </div>
   </div>
 </template>
