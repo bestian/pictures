4c35bde0b2ce5b921fa1f621f2e3f206736f4456
Bestian Tang <bestian@gmail.com>
2022-09-30 21:22:12 +0800

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,6 +4,7 @@
     <div class="ui segment container">
       <img class = "floated left" src="../assets/m0.jpg" />
       <p>耀洋的小名叫作小太陽。是一個活潑、溫暖、開朗的小孩，健康靈活，喜歡和人互動相處，無論是小小孩、同齡朋友、青年、成人、老人，他都樂於相處互動。</p>
+      <p>小太陽在家裡時常做一些東西，例如秘密基地。</p>
     </div>
   </div>
 </template>
